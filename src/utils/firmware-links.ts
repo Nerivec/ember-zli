b5d205283c2c30235d4b2c6e078d3386b0215164
Nerivec <62446222+Nerivec@users.noreply.github.com>
2024-10-29 19:04:31 +0100

    Update firmware links.

--- src/utils/firmware-links.ts
+++ src/utils/firmware-links.ts
@@ -47,6 +47,11 @@ export const FIRMWARE_LINKS: Record<FirmwareVariant, Record<AdapterModel, Firmwa
             url: 'https://github.com/darkxst/silabs-firmware-builder/raw/refs/heads/main/firmware_builds/zbdonglee/ncp-uart-hw-v7.4.4.0-zbdonglee-115200.gbl',
             version: '7.4.4.0',
         },
+        'Sonoff ZBDongle-E - ROUTER': {
+            settings: { baudRate: 115200, rtscts: false },
+            url: `https://github.com/itead/Sonoff_Zigbee_Dongle_Firmware/raw/refs/heads/master/Dongle-E/Router/Z3RouterUSBDonlge_EZNet6.10.3_V1.0.0.gbl`,
+            version: '6.10.3.0',
+        },
         'SparkFun MGM240p': {
             settings: { baudRate: 115200, rtscts: false },
             url: 'https://github.com/darkxst/silabs-firmware-builder/raw/refs/heads/main/firmware_builds/mgm240p/ncp-uart-hw-v7.4.4.0-mgm240p-115200.gbl',
@@ -108,6 +113,11 @@ export const FIRMWARE_LINKS: Record<FirmwareVariant, Record<AdapterModel, Firmwa
             settings: { baudRate: 115200, rtscts: false },
             url: 'https://github.com/darkxst/silabs-firmware-builder/raw/refs/heads/main/firmware_builds/zbdonglee/ncp-uart-hw-v7.4.4.0-zbdonglee-115200.gbl',
             version: '7.4.4.0',
+        },
+        'Sonoff ZBDongle-E - ROUTER': {
+            settings: { baudRate: 115200, rtscts: false },
+            url: `https://github.com/itead/Sonoff_Zigbee_Dongle_Firmware/raw/refs/heads/master/Dongle-E/Router/Z3RouterUSBDonlge_EZNet6.10.3_V1.0.0.gbl`,
+            version: '6.10.3.0',
         },
         'SparkFun MGM240p': {
             settings: { baudRate: 115200, rtscts: false },
@@ -171,6 +181,11 @@ export const FIRMWARE_LINKS: Record<FirmwareVariant, Record<AdapterModel, Firmwa
             url: 'https://github.com/itead/Sonoff_Zigbee_Dongle_Firmware/raw/refs/heads/master/Dongle-E/NCP_7.4.3/ncp-uart-sw_EZNet7.4.3_V1.0.0.gbl',
             version: '7.4.3.0',
         },
+        'Sonoff ZBDongle-E - ROUTER': {
+            settings: { baudRate: 115200, rtscts: false },
+            url: `https://github.com/itead/Sonoff_Zigbee_Dongle_Firmware/raw/refs/heads/master/Dongle-E/Router/Z3RouterUSBDonlge_EZNet6.10.3_V1.0.0.gbl`,
+            version: '6.10.3.0',
+        },
         'SparkFun MGM240p': {
             settings: { baudRate: 115200, rtscts: false },
             url: undefined,
@@ -190,62 +205,67 @@ export const FIRMWARE_LINKS: Record<FirmwareVariant, Record<AdapterModel, Firmwa
     experimental: {
         'Aeotec Zi-Stick (ZGA008)': {
             settings: { baudRate: 115200, rtscts: true },
-            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2/aeotec_zga008_ncp-uart-hw_115200_8.0.2.0.gbl`,
+            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2-update1/aeotec_zga008_zigbee_ncp_8.0.2.0_115200.gbl`,
             version: '8.0.2.0',
         },
         'EasyIOT ZB-GW04 v1.1': {
             settings: { baudRate: 115200, rtscts: false },
-            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2/easyiot_zb-gw04-1v1_ncp-uart-sw_115200_8.0.2.0.gbl`,
+            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2-update1/easyiot_zb-gw04-1v1_zigbee_ncp_8.0.2.0_115200_sw_flow.gbl`,
             version: '8.0.2.0',
         },
         'EasyIOT ZB-GW04 v1.2': {
             settings: { baudRate: 115200, rtscts: true },
-            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2/easyiot_zb-gw04-1v2_ncp-uart-hw_115200_8.0.2.0.gbl`,
+            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2-update1/easyiot_zb-gw04-1v2_zigbee_ncp_8.0.2.0_115200.gbl`,
             version: '8.0.2.0',
         },
         'Home Assistant SkyConnect': {
             settings: { baudRate: 115200, rtscts: true },
-            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2/nabucasa_skyconnect_ncp-uart-hw_115200_8.0.2.0.gbl`,
+            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2-update1/nabucasa_skyconnect_zigbee_ncp_8.0.2.0_115200.gbl`,
             version: '8.0.2.0',
         },
         'Home Assistant Yellow': {
             settings: { baudRate: 115200, rtscts: true },
-            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2/nabucasa_yellow_ncp-uart-hw_115200_8.0.2.0.gbl`,
+            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2-update1/nabucasa_yellow_zigbee_ncp_8.0.2.0_115200.gbl`,
             version: '8.0.2.0',
         },
         'SMLight SLZB06-M': {
             settings: { baudRate: 115200, rtscts: false },
-            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2/smlight_slzb06m_ncp-uart-sw_115200_8.0.2.0.gbl`,
+            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2-update1/smlight_slzb06m_zigbee_ncp_8.0.2.0_115200_sw_flow.gbl`,
             version: '8.0.2.0',
         },
         'SMLight SLZB07': {
             settings: { baudRate: 115200, rtscts: true },
-            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2/smlight_slzb07_ncp-uart-hw_115200_8.0.2.0.gbl`,
+            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2-update1/smlight_slzb07_zigbee_ncp_8.0.2.0_115200.gbl`,
             version: '8.0.2.0',
         },
         'SMLight SLZB07mg24': {
             settings: { baudRate: 115200, rtscts: true },
-            url: undefined,
+            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2-update1/smlight_slzb07Mg24_zigbee_ncp_8.0.2.0_115200.gbl`,
             version: '8.0.2.0',
         },
         'Sonoff ZBDongle-E': {
             settings: { baudRate: 115200, rtscts: false },
-            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2/sonoff_zbdonglee_ncp-uart-sw_115200_8.0.2.0.gbl`,
+            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2-update1/sonoff_zbdonglee_zigbee_ncp_8.0.2.0_115200_sw_flow.gbl`,
+            version: '8.0.2.0',
+        },
+        'Sonoff ZBDongle-E - ROUTER': {
+            settings: { baudRate: 115200, rtscts: false },
+            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2-update1/sonoff_zbdonglee_zigbee_router_8.0.2.0_115200_sw_flow.gbl`,
             version: '8.0.2.0',
         },
         'SparkFun MGM240p': {
             settings: { baudRate: 115200, rtscts: false },
-            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2/sparkfun_mgm240p_ncp-uart-sw_115200_8.0.2.0.gbl`,
+            url: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2-update1/sparkfun_mgm240p_zigbee_ncp_8.0.2.0_115200_sw_flow.gbl`,
             version: '8.0.2.0',
         },
         'TubeZB MGM24': {
             settings: { baudRate: 115200, rtscts: true },
-            url: undefined, // NOT WORKING: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2/tubeszb_mgm24_ncp-uart-hw_115200_8.0.2.0.gbl`,
+            url: undefined, // NOT WORKING: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2-update1/tubeszb-mgm24-zigbee_ncp_8.0.2.0_115200.gbl`,
             version: '8.0.2.0',
         },
         'TubeZB MGM24PB': {
             settings: { baudRate: 115200, rtscts: true },
-            url: undefined,
+            url: undefined, // NOT WORKING: `https://github.com/Nerivec/silabs-firmware-builder/releases/download/v2024.6.2-update1/tubeszb-mgm24PB-zigbee_ncp_8.0.2.0_115200.gbl`,
             version: '8.0.2.0',
         },
     },
