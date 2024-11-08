fd1f0b4856874a273b66adf81493bdbb6e792906
Nerivec <62446222+Nerivec@users.noreply.github.com>
2024-11-09 00:49:00 +0100

    Improve monitor output and log to usual file

--- src/commands/monitor/index.ts
+++ src/commands/monitor/index.ts
@@ -1,5 +1,7 @@
 import { Command } from '@oclif/core'
 
+import { SLStatus } from 'zigbee-herdsman/dist/adapter/ember/enums.js'
+
 import { logger } from '../../index.js'
 import { getPortConf } from '../../utils/port.js'
 import { Transport, TransportEvent } from '../../utils/transport.js'
@@ -8,6 +10,8 @@ export default class Monitor extends Command {
     static override args = {}
     static override description = 'Monitor the chosen port in the console.'
     static override examples = ['<%= config.bin %> <%= command.id %>']
+
+    private logBuffer: Buffer = Buffer.alloc(0)
 
     public async run(): Promise<void> {
         const portConf = await getPortConf()
@@ -28,7 +32,7 @@ export default class Monitor extends Command {
         logger.info(`Started monitoring. Press any key to stop.`)
 
         transport.on(TransportEvent.FAILED, () => this.exit(1))
-        transport.on(TransportEvent.DATA, (data) => process.stdout.write(data))
+        transport.on(TransportEvent.DATA, this.onTransportData.bind(this))
 
         process.stdin.setRawMode(true)
         process.stdin.resume()
@@ -41,5 +45,37 @@ export default class Monitor extends Command {
         })
 
         return this.exit(0)
+    }
+
+    private onTransportData(received: Buffer): void {
+        // concat received to previous to ensure lines are outputted properly
+        let data = Buffer.concat([this.logBuffer, received])
+        let position: number
+
+        while ((position = data.indexOf('\r\n')) !== -1) {
+            // take everything up to '\r\n' (excluded)
+            const line = data.subarray(0, position)
+
+            // skip blank lines
+            if (line.length > 0) {
+                let asciiLine = line.toString('ascii')
+                // append SLStatus at end of line if detected hex for it
+                //   - "Join network complete: 0x18"
+                //   - "Join network start: 0x0"
+                // XXX: format seems pretty standard throughout the SDK, but this might create some false matches (hence leaving the hex too)
+                const endStatusMatch = asciiLine.match(/ (0x\d+)$/)
+
+                if (endStatusMatch) {
+                    asciiLine += ` (${SLStatus[Number.parseInt(endStatusMatch[1], 16)]})`
+                }
+
+                logger.info(asciiLine)
+            }
+
+            // remove the line from internal buffer (set below), this time include '\r\n'
+            data = data.subarray(position + 2)
+        }
+
+        this.logBuffer = data
     }
 }
